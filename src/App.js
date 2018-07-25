fb96be198893e8d7f724db728278b188f9ea8e59
Justin Lee <leejwg.3@gmail.com>
2018-07-25 02:47:42 -0230

    more css added crowd rank basic

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Nav from "./components/Nav/Nav";
 import Logo from "./components/Logo/Logo";
 import ImageForm from "./components/ImageForm/ImageForm";
 import FacialRec from "./components/FacialRec/FacialRec";
+import CrowdRank from "./components/CrowdRank/CrowdRank";
 import "./App.css";
 
 class App extends Component {
@@ -12,6 +13,7 @@ class App extends Component {
       <div className="App">
         <Nav />
         <Logo />
+        <CrowdRank />
         <ImageForm />
         <FacialRec />
       </div>
---
+++ src/components/CrowdRank/CrowdRank.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CrowdRank = () => {
+  return <div className="white f3">{"You stack up at #2"}</div>;
+};
+
+export default CrowdRank;
