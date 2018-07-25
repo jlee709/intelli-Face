5b3dda057f0bcbc65263482505fbffbd43afb43f
Justin Lee <leejwg.3@gmail.com>
2018-07-25 02:35:04 -0230

    added styles and class to buttons and added some css files and classes

--- src/components/ImageForm/ImageForm.js
+++ src/components/ImageForm/ImageForm.js
@@ -1,9 +1,19 @@
 import React from "react";
+import "./ImageForm.css";
 
 const ImageForm = () => {
   return (
     <div>
       <p className="f3">{"IntelliFace Can Recognize Faces from Images~"}</p>
+
+      <div className="center">
+        <div className="form center pa4 br3 shadow-5">
+          <input className="f4 pa2 w-70 center" type="tex" />
+          <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-green">
+            Scan
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
