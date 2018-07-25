0ae192823b48f44421f33dc3062b73b89a450504
Justin Lee <leejwg.3@gmail.com>
2018-07-25 00:15:40 -0230

    added logo css to match tilt image

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Tilt from "react-tilt";
+import "./Logo.css";
 
 const Logo = () => {
   return (
     <div className="ma4 mt0">
       <Tilt
-        className="Tilt br2 shadow-3"
-        options={{ max: 25 }}
+        className="Tilt br shadow-2"
+        options={{ max: 55 }}
         style={{ height: 200, width: 200 }}
       >
         <div className="Tilt-inner"> 👽 </div>
