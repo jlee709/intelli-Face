6f7ae3a35eb7d534f2fd5cf0cee42c899fd79c17
Justin Lee <leejwg.3@gmail.com>
2018-08-02 14:33:08 +0900

    added particles to back ground

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Particles from "react-particles-js";
 
 import Nav from "./components/Nav/Nav";
 import Logo from "./components/Logo/Logo";
@@ -7,10 +8,23 @@ import FacialRec from "./components/FacialRec/FacialRec";
 import CrowdRank from "./components/CrowdRank/CrowdRank";
 import "./App.css";
 
+const particleOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+};
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className="particles" params={particleOptions} />
         <Nav />
         <Logo />
         <CrowdRank />
