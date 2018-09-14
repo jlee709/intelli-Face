5279d8c5641f7f1793dc31e59e665ae516694f18
Justin Lee <leejwg.3@gmail.com>
2018-09-14 13:47:11 +0900

    basic posts working with bp url and json

--- server/server.js
+++ server/server.js
@@ -1,10 +1,53 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-// app.get("/", (req, res) => {
-//   res.send("we are operational");
-// });
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+app.use(bodyParser.json());
+
+const database = {
+  users: [
+    {
+      id: 23,
+      name: "justin",
+      password: "hello",
+      email: "justin@jlee.com",
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: 24,
+      name: "wayne",
+      password: "karate",
+      email: "wayne@jlee.com",
+      entries: 2,
+      joined: new Date()
+    }
+  ]
+};
+
+app.get("/", (req, res) => {
+  res.send("we are operational");
+});
+
+app.post("/signin", (req, res) => {
+  // console.log(req.body.email);
+  // console.log(database.users[0].password);
+  if (
+    req.body.password === database.users[0].password &&
+    req.body.email === database.users[0].email
+  ) {
+    res.json("Succsess!!");
+  } else {
+    res.status(400).json("login error");
+  }
+});
 
 app.listen(3000, () => {
   console.log("servers up");
